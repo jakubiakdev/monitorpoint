0ec0a9196966e110241245387aabce69e82b5297
jakubiakdev <jakubiakdev@gmail.com>
2022-06-30 16:14:55 +0200

    Fix typo

--- routes/info.js
+++ routes/info.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const packageJson = require('../package.json')
 
 router.get('/', function(req, res) {
-    res.send(`This service uses MonitorPoint ${packageJson.version} (avalible at ${packageJson.homepage}). MonitorPoint is licenced under AGPL 3.0 or later. © Jakubiakdev and contributors 2022.`)
+    res.send(`This service uses MonitorPoint ${packageJson.version} (available at ${packageJson.homepage}). MonitorPoint is licensed under AGPL 3.0 or later. © Jakubiakdev and contributors 2022.`)
 })
 
 module.exports = router
