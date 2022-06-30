e88aa390ea4198efdeeb3df5785e62a4365b682c
jakubiakdev <jakubiakdev@gmail.com>
2022-06-30 16:59:32 +0200

    Memory used sounds better

--- routes/memory.js
+++ routes/memory.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const si = require('systeminformation');
 
-router.get('/usage', async function(req, res) {
+router.get('/used', async function(req, res) {
     res.set('Content-Type', 'text/plain');
     let percentage = ((await si.mem()).used / (await si.mem()).total) * 100
     res.send(`${percentage}`)
