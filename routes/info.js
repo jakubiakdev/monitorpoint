885fa0146ade5bdea5188c74186dbaaa9ad093ae
jakubiakdev <jakubiakdev@gmail.com>
2022-06-30 16:58:32 +0200

    Add content type to info

--- routes/info.js
+++ routes/info.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const packageJson = require('../package.json')
 
 router.get('/', function(req, res) {
+    res.set('Content-Type', 'text/plain');
     res.send(`This service uses MonitorPoint ${packageJson.version} (available at ${packageJson.homepage}). MonitorPoint is licensed under AGPL 3.0 or later. © Jakubiakdev and contributors 2022.`)
 })
 
